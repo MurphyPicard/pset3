a486ca19f9deeb424d939a7cf6edf8297348fe9e
Ara <ayaghsizian@gmail.com>
2017-11-12 19:32:08 +0000

    added notes to helpers.c and about to make search function

--- find/helpers.c
+++ find/helpers.c
@@ -1,19 +1,23 @@
-/**
- * helpers.c
- *
- * Helper functions for Problem Set 3.
- */
- 
+#include <stdio.h>
 #include <cs50.h>
-
 #include "helpers.h"
 
-/**
- * Returns true if value is in array of n values, else false.
- */
+// BINARY SEARCH // Divide and Conquer, The array must be sorted first.
+// Worst - O(log n)
+// Best - Omega(1)
+
+// Repeat until the (sub)array is size 0:
+//     Calculate the middle point of the (sub)array.
+//     If the target is at the middle, stop.
+//     Otherwise, if the target is less than what's at the middle, repeat, changing endpoint to be left of middle.
+//     Otherwise, if the target is greater than what's at the middle, repeat, changing endpoint to be right of middle.
+
+// Returns true if value is in array of n values, else false.
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
+
+    int leftindex = 0;
+    int rightindex = n - 1;
     return false;
 }
 
@@ -23,5 +27,9 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement a sorting algorithm
+    while ()
+    {
+
+    }
     return;
 }
